75f243124b4406198b43fac9bc3de33eb05dd9eb
roberto.martin <roberto.martin@udc.es>
2016-04-04 19:32:58 +0200

    Segundo commit

--- iFinances/jpa-dao/src/main/java/es/udc/fic/iFinances/dao/main/Main.java
+++ iFinances/jpa-dao/src/main/java/es/udc/fic/iFinances/dao/main/Main.java
@@ -12,43 +12,33 @@ import org.springframework.transaction.TransactionStatus;
 
 import es.udc.fic.iFinances.dao.department.Department;
 import es.udc.fic.iFinances.dao.department.DepartmentDao;
-import es.udc.fic.iFinances.dao.employee.Employee;
-import es.udc.fic.iFinances.modelutil.exceptions.InstanceNotFoundException;
 public class Main {
 
 	public static void main(String[] args) throws Throwable {
 		
-		PlatformTransactionManager transactionManager;
+		//PlatformTransactionManager transactionManager;
 		@SuppressWarnings("resource")
-		ApplicationContext ctx = new ClassPathXmlApplicationContext(SPRING_CONFIG_FILE);
+		ApplicationContext context = new ClassPathXmlApplicationContext(SPRING_CONFIG_FILE);
 		
-		transactionManager = ctx.getBean(PlatformTransactionManager.class);
-		
-		TransactionStatus transactionStatus =
-	            transactionManager.getTransaction(null);
-		
-		DepartmentDao departmentDao= DepartmentDao.class.cast(ctx.getBean("departmentDao"));
+		PlatformTransactionManager transactionManager = context.getBean(PlatformTransactionManager.class);
+        
+        DepartmentDao departmentDao = context.getBean(DepartmentDao.class);
+        TransactionStatus transactionStatus =
+                transactionManager.getTransaction(null);
 		try {
 			
 		
 		Department ict = new Department(
                 "Department of Information and Communications Technologies",
                 Calendar.getInstance());
-            /*Employee fbellas = new Employee("Fernando", "Bellas Permuy", "tu",
-                200);
-            Employee jrs = new Employee("Juan", "Raposo Santiago", "cd", 200);
-            Employee mad = new Employee("Manuel", "Alvarez Diaz", "cd", 200);
-            Employee jss = new Employee("Jose", "Soto Soto", "tu", 300);
-            */
          
          departmentDao.save(ict);
-         Department d= departmentDao.find(ict.getDepartmentId());
-         
-         System.out.println("aaaa");
          transactionManager.commit(transactionStatus);
+    
+        System.out.println("aaaa");
 
         } catch (Throwable e) {
-            transactionManager.rollback(transactionStatus);
+           // transactionManager.rollback(transactionStatus);
             throw e;
         }
 		
